018ef8cbad753d62fba427b25afd03b4b22bbb75
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2012-08-22 15:25:31 +0200

    [openssl] bugfix on openssl::ca::sign which now declare the file resource for the generated certificate
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- manifests/definitions/openssl-ca-sign.pp
+++ manifests/definitions/openssl-ca-sign.pp
@@ -44,91 +44,66 @@ define openssl::ca::sign (
         default => "${csr}"
     }
     $certfile   = "${basedir}/${certname}${openssl::params::cert_filename_suffix}"
+    $pubkeyfile = "${basedir}/${certname}${openssl::params::pubkey_filename_suffix}"
 
     info ("Sign the ${certname} certficate via openssl::ca::sign (in ${basedir})")
-
-    # First eventually copy the Certificate Signing Request (CSR) to the CA
-    # directory
-    exec { "copy ${csrfile} into ${cadir}":
-        command => "cp ${csrfile} ${cadir}/${certname}${openssl::params::csr_filename_suffix}",
-        path    => "/usr/bin:/usr/sbin/:/bin:/sbin",
-        onlyif  => "test -f ${csrfile}",
-        user    => "${owner}",
-        group   => "${group}",
-        unless  => "test -f ${basedir}/${certname}${openssl::params::cert_filename_suffix}",
-        require => File["${cadir}"]
-    }
 
     # Sign the CSR
     $create_signing_authority_opts = $create_signing_authority ? {
         true    => "-extfile openssl.cnf -extensions v3_ca",
         default => ''
     }
-    
+
     exec { "Sign ${certname} Certificate Signing Request (CSR)":
         path    => "/usr/bin:/usr/sbin/:/bin:/sbin",
         user    => "${owner}",
         group   => "${group}",
         cwd     => "${cadir}",
-        command => "openssl ca -batch -config openssl.cnf ${create_signing_authority_opts} -in ${certname}${openssl::params::csr_filename_suffix} -out signed/${certname}${openssl::params::cert_filename_suffix}",
-        unless  => "test -f ${basedir}/${certname}${openssl::params::cert_filename_suffix}",
+        command => "openssl ca -batch -config openssl.cnf ${create_signing_authority_opts} -in ${csrfile} -out ${certfile}",
+        onlyif  => "test -f ${csrfile}",
+        creates => "${certfile}",
         require => [
                     Package['openssl'],
                     Openssl::Ca::Init["${cadir}"],
                     File["${cadir}/openssl.cnf"],
-                    File["${cadir}/signed"],
-                    Exec["copy ${csrfile} into ${cadir}"]
                     ]
     }
 
-    
-    # Clean the environment 
-    exec { "rm ${cadir}/${certname}${openssl::params::csr_filename_suffix}":
-        path    => "/usr/bin:/usr/sbin/:/bin:/sbin",
-        onlyif  => "test -f ${cadir}/${certname}${openssl::params::csr_filename_suffix}",
-        require => Exec["Sign ${certname} Certificate Signing Request (CSR)"]
-    }
-    exec { "rm ${basedir}/${certname}${openssl::params::csr_filename_suffix}":
-        path    => "/usr/bin:/usr/sbin/:/bin:/sbin",
-        onlyif  => "test -f ${basedir}/${certname}${openssl::params::csr_filename_suffix}",
-        require => Exec["Sign ${certname} Certificate Signing Request (CSR)"]
-    }
-     
-    # Prepare the layout in basedir
-    exec { "cp ${cadir}/signed/${certname}${openssl::params::cert_filename_suffix} ${basedir}/":
-        path    => "/usr/bin:/usr/sbin/:/bin:/sbin",
-        user    => "${owner}",
-        group   => "${group}",
-        cwd     => "${cadir}",
-        onlyif  => "test -f ${cadir}/signed/${certname}${openssl::params::cert_filename_suffix}",
-        require => Exec["Sign ${certname} Certificate Signing Request (CSR)"]
-    }
-    file { "${cadir}/signed/${certname}.pem":
-        ensure  => 'link',
+    file { "${certfile}":
+        ensure  => 'file',
         owner   => "${owner}",
         group   => "${group}",
-        target  => "${cadir}/signed/${certname}${openssl::params::cert_filename_suffix}",
-        require => Exec["Sign ${certname} Certificate Signing Request (CSR)"]     
+        require => Exec["Sign ${certname} Certificate Signing Request (CSR)"]
     }
 
-    # file { "${basedir}/${certname}.pem":
-    #     ensure  => 'link',
-    #     target  => "${certname}${openssl::params::cert_filename_suffix}",
-    #     require => Exec["Sign ${certname} Certificate Signing Request (CSR)"]     
-    # }
-   
-    exec { "openssl x509 -in ${certname}${openssl::params::cert_filename_suffix} -pubkey -noout > ${certname}${openssl::params::pubkey_filename_suffix}":
+    # Clean the environement i.e. delete the CSR file
+    exec { "rm ${csrfile}":
+        path    => "/usr/bin:/usr/sbin/:/bin:/sbin",
+        onlyif  => "test -f ${csrfile}",
+        require => Exec["Sign ${certname} Certificate Signing Request (CSR)"]
+    }
+
+    # Also, generate the public key file (compliance with Puppet built-in CA - I
+    # still don't catch why on earth they take care of public key files while
+    # they have certificates...
+    exec { "Generate ${pubkeyfile}":
+        command => "openssl x509 -in ${certfile} -pubkey -noout > ${certname}${openssl::params::pubkey_filename_suffix}",
         path    => "/usr/bin:/usr/sbin/:/bin:/sbin",
         cwd     => "${basedir}",
         user    => "${owner}",
         group   => "${group}",
         onlyif  => "test -f ${basedir}/${certname}${openssl::params::cert_filename_suffix}",
-        unless  => "test -f ${basedir}/${certname}${openssl::params::pubkey_filename_suffix}", 
-        require => Exec["cp ${cadir}/signed/${certname}${openssl::params::cert_filename_suffix} ${basedir}/"]
+        creates => "${basedir}/${certname}${openssl::params::pubkey_filename_suffix}",
+        require => File["${certfile}"]
     }
 
-    
+     file { "${pubkeyfile}":
+        ensure  => 'file',
+        owner   => "${owner}",
+        group   => "${group}",
+        require => Exec["Generate ${pubkeyfile}"]
+    }
+   
 
     
-
 }
